293202b92ec9a528bc572adb65d4498c489ca7cf
Paul Sorensen <paulnsorensen@gmail.com>
2013-08-13 22:01:56 -0500

    Fixed railtie to load correctly

--- lib/active_subset_validator/railtie.rb
+++ lib/active_subset_validator/railtie.rb
@@ -1,8 +1,8 @@
 module ActiveSubsetValidator
   class Railtie < Rails::Railtie
-    initializer 'active_subset_validator.subset_validator' do
+    initializer 'active_subset_validator' do
       ActiveSupport.on_load :active_record do
-        extend SubsetValidator
+        include ActiveSubsetValidator
       end
     end
   end
